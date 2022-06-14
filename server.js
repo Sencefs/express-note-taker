8b1f478e85fb13e5b7ba7c876587e8cb9b19808b
Sencefs <100245563+Sencefs@users.noreply.github.com>
2022-06-14 16:15:09 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,66 @@
+//Setting up express and listing dependencies
+const PORT = process.env.PORT || 3001;
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const app = express();
+const allNotes = require('./db/db.json');
+//Linking assets
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+app.get('/api/notes', (req, res) => {
+    res.json(allNotes.slice(1));
+});
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+//Notes html and its URL
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+//Create new note
+function createNewNote(body, notesArray) {
+    const newNote = body;
+    if (!Array.isArray(notesArray))
+        notesArray = [];
+    if (notesArray.length === 0)
+        notesArray.push(0);
+    body.id = notesArray[0];
+    notesArray[0]++;
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    return newNote;
+}
+app.post('/api/notes', (req, res) => {
+    const newNote = createNewNote(req.body, allNotes);
+    res.json(newNote);
+});
+//Request to delete note by ID
+function deleteNote(id, notesArray) {
+    for (let i = 0; i < notesArray.length; i++) {
+        let note = notesArray[i];
+        if (note.id == id) {
+            notesArray.splice(i, 1);
+            fs.writeFileSync(
+                path.join(__dirname, './db/db.json'),
+                JSON.stringify(notesArray, null, 2)
+            );
+            break;
+        }
+    }
+}
+app.delete('/api/notes/:id', (req, res) => {
+    deleteNote(req.params.id, allNotes);
+    res.json(true);
+});
+//Set up server
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
